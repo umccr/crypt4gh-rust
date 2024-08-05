3b1f49fc439eca5bd9b68278100cc7bee4bb22e5
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-08-05 16:09:44 +1000

    Fix CRAM header reading, drafting the public API

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
 pub mod header;
 pub mod keys;
 pub mod error;
-
-struct Crypt4Gh {
-
-}
-
-impl Crypt4Gh {
-    async fn new() {
-        unimplemented!()
-    }
-}
+pub mod client;
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 use std::{error::Error, path::PathBuf};
 
-use crypt4gh::{error::Crypt4GHError, keys::{EncryptionMethod, KeyPair, PrivateKey, PublicKey}};
+use crypt4gh::error::Crypt4GHError;
+use crypt4gh::keys::{EncryptionMethod, KeyPair, PrivateKey, PublicKey};
 use noodles::cram;
+use tokio::fs::File;
 
-async fn read_cram(path: PathBuf) -> Result<Vec<u8>, Crypt4GHError> {
-    let mut reader = cram::r#async::io::reader::Builder::default()
-        .set_reference_sequence_repository(repository)
-        .build_from_path(path)
-        .await?;
-
-    let header = reader.read_header().await?;
-    let mut records = reader.records(&header);
-
-    // TODO: Read (whole?) payload, not just header
-
-    Ok(records)
+async fn read_cram_header(src: PathBuf) -> Result<String, Crypt4GHError> {
+    let mut reader = File::open(src).await.map(cram::AsyncReader::new)?;
+    let header = reader.read_file_header().await?;
+    Ok(header)
 }
 
 #[tokio::main]
@@ -23,19 +17,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let pubkey = PublicKey::new();
     let privkey = PrivateKey::new();
     let keys = KeyPair::new(EncryptionMethod::X25519Chacha20Poly305, privkey, pubkey);
-    
+
     // Init the Crypt4GH client
-    let c4gh = crypt4gh::new();
-    
-    // Read bytes from stdin for a Crypt4GH encrypted BAM/CRAM
-    let plain = read_cram(PathBuf::from("./data/cram/htsnexus_test_NA12878.cram")).await;
+    let c4gh = Crypt4Gh::new(keys);
+
+    // Read bytes from stdin for a CRAM
+    let plain = read_cram_header(PathBuf::from("./data/cram/htsnexus_test_NA12878.cram")).await?.as_bytes().to_vec();
 
     // Encrypt and decrypt payload
-    let enc = c4gh.encrypt(plain, keys);
-    let dec = c4gh.decrypt(enc, keys);
-    
+    let enc = c4gh.encrypt(plain)?;
+    let dec = c4gh.decrypt(enc)?;
+
     // Make sure it worked
-    assert!(plain, dec);
+    assert_eq!(plain, dec);
 
     // All is fine
     Ok(())
