684113782c1a3332dd6d93bc5ed60a08528deac6
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-09-04 11:58:25 +1000

    reader.read_file_definition() is crucial for noodles to read (seek) within the CRAM header before calling reader.read_file_header()

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use tokio::fs::File;
 
 async fn read_cram_header(src: PathBuf) -> Result<String, Crypt4GHError> {
 	let mut reader = File::open(src).await.map(cram::AsyncReader::new)?;
+    reader.read_file_definition().await?;
 	let header = reader.read_file_header().await?;
 	Ok(header)
 }
@@ -26,23 +27,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let c4gh = Crypt4Gh::new(keys.clone());
 
 	// Read bytes from stdin for a CRAM
-	let plain = read_cram_header(PathBuf::from("./data/cram/htsnexus_test_NA12878.cram"))
+	let plain = read_cram_header(PathBuf::from("./data/cram/range.cram"))
 		.await?
 		.as_bytes()
 		.to_vec();
 	let plain = PlainText::from(plain);
 
 	// Encrypt and decrypt payload
-	//let m = c4gh.clone();
 	let enc = c4gh.encrypt(plain)?;
 	let dec = enc.decrypt(keys)?;
-	//let dec = m.decrypt(enc)?;
 
-	//dbg!(&plain);
 	dbg!(dec);
-
-	// Make sure it worked
-	//assert_eq!(plain, dec);
 
 	// All is fine
 	Ok(())
